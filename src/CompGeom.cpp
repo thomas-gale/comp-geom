cecda87bfb7e752b8d743c6bff6bacedbc77515a
Thomas Gale <gale.thomasjames@gmail.com>
2020-11-05 18:18:48 +0000

    Added basic primitive cube to comp geom example.

--- src/CompGeom.cpp
+++ src/CompGeom.cpp
@@ -1,41 +1,80 @@
+#include <Magnum/GL/Buffer.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
-#include <Magnum/Platform/Sdl2Application.h>
-
-#include <Corrade/Utility/DebugStl.h>
-#include <Magnum/GL/Context.h>
+#include <Magnum/GL/Mesh.h>
 #include <Magnum/GL/Renderer.h>
-#include <Magnum/GL/Version.h>
 #include <Magnum/Math/Color.h>
+#include <Magnum/Math/Matrix4.h>
+#include <Magnum/MeshTools/CompressIndices.h>
+#include <Magnum/MeshTools/Interleave.h>
+#include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Primitives/Cube.h>
+#include <Magnum/Shaders/Phong.h>
+#include <Magnum/Trade/MeshData.h>
 
 using namespace Magnum;
+using namespace Math::Literals;
 
 class CompGeom : public Platform::Application {
-public:
-  explicit CompGeom(const Arguments &arguments);
+  public:
+    explicit CompGeom(const Arguments& arguments);
 
-private:
-  void drawEvent() override;
+  private:
+    void drawEvent() override;
+
+    GL::Mesh _mesh;
+    Shaders::Phong _shader;
+
+    Matrix4 _transformation, _projection;
+    Color3 _color;
 };
 
-CompGeom::CompGeom(const Arguments &arguments)
-    : Platform::Application{arguments} {
-  // TODO: Add your initialization code here
+CompGeom::CompGeom(const Arguments& arguments)
+    : Platform::Application{arguments, Configuration{}.setTitle("Comp Geom")} {
+    GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+    GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
 
-  using namespace Math::Literals;
+    Trade::MeshData cube = Primitives::cubeSolid();
 
-  GL::Renderer::setClearColor(0xa5c9ea_rgbf);
+    GL::Buffer vertices;
+    vertices.setData(MeshTools::interleave(cube.positions3DAsArray(),
+                                           cube.normalsAsArray()));
 
-  Debug{} << "Hello! This application is running on"
-          << GL::Context::current().version() << "using"
-          << GL::Context::current().rendererString();
+    std::pair<Containers::Array<char>, MeshIndexType> compressed =
+        MeshTools::compressIndices(cube.indicesAsArray());
+    GL::Buffer indices;
+    indices.setData(compressed.first);
+
+    _mesh.setPrimitive(cube.primitive())
+        .setCount(cube.indexCount())
+        .addVertexBuffer(std::move(vertices), 0, Shaders::Phong::Position{},
+                         Shaders::Phong::Normal{})
+        .setIndexBuffer(std::move(indices), 0, compressed.second);
+
+    // This is a verbose version of:
+    //_mesh = MeshTools::compile(Primitives::cubeSolid());
+
+    _transformation =
+        Matrix4::rotationX(30.0_degf) * Matrix4::rotationY(40.0_degf);
+    _projection =
+        Matrix4::perspectiveProjection(
+            35.0_degf, Vector2{windowSize()}.aspectRatio(), 0.01f, 100.0f) *
+        Matrix4::translation(Vector3::zAxis(-10.0f));
+    _color = Color3::fromHsv({35.0_degf, 1.0f, 1.0f});
 }
 
 void CompGeom::drawEvent() {
-  GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
+    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color |
+                                 GL::FramebufferClear::Depth);
 
-  // TODO: Add your drawing code here
+    _shader.setLightPositions({{7.0f, 5.0f, 2.5f, 0.0f}})
+        .setDiffuseColor(_color)
+        .setAmbientColor(Color3::fromHsv({_color.hue(), 1.0f, 0.3f}))
+        .setTransformationMatrix(_transformation)
+        .setNormalMatrix(_transformation.normalMatrix())
+        .setProjectionMatrix(_projection)
+        .draw(_mesh);
 
-  swapBuffers();
+    swapBuffers();
 }
 
 MAGNUM_APPLICATION_MAIN(CompGeom)
