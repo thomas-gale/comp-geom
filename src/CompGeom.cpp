b2903660b4dccbdacfd094cbbab4218ba9fa3ebc
Thomas Gale <gale.thomasjames@gmail.com>
2020-11-05 15:48:48 +0000

    Experimentally setClearColor.

--- src/CompGeom.cpp
+++ src/CompGeom.cpp
@@ -1,28 +1,41 @@
 #include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/Platform/Sdl2Application.h>
 
+#include <Corrade/Utility/DebugStl.h>
+#include <Magnum/GL/Context.h>
+#include <Magnum/GL/Renderer.h>
+#include <Magnum/GL/Version.h>
+#include <Magnum/Math/Color.h>
+
 using namespace Magnum;
 
-class CompGeom: public Platform::Application {
-    public:
-        explicit CompGeom(const Arguments& arguments);
+class CompGeom : public Platform::Application {
+public:
+  explicit CompGeom(const Arguments &arguments);
 
-    private:
-        void drawEvent() override;
+private:
+  void drawEvent() override;
 };
 
-CompGeom::CompGeom(const Arguments& arguments):
-    Platform::Application{arguments}
-{
-    // TODO: Add your initialization code here
+CompGeom::CompGeom(const Arguments &arguments)
+    : Platform::Application{arguments} {
+  // TODO: Add your initialization code here
+
+  using namespace Math::Literals;
+
+  GL::Renderer::setClearColor(0xa5c9ea_rgbf);
+
+  Debug{} << "Hello! This application is running on"
+          << GL::Context::current().version() << "using"
+          << GL::Context::current().rendererString();
 }
 
 void CompGeom::drawEvent() {
-    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
+  GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
 
-    // TODO: Add your drawing code here
+  // TODO: Add your drawing code here
 
-    swapBuffers();
+  swapBuffers();
 }
 
 MAGNUM_APPLICATION_MAIN(CompGeom)
