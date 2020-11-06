b0ed910550d0589e01bb6b8761a1d44d0c5db36b
Thomas Gale <gale.thomasjames@gmail.com>
2020-11-06 14:25:57 +0000

    Rendering axis and grid.

--- src/CompGeom.cpp
+++ src/CompGeom.cpp
@@ -8,6 +8,7 @@
 #include <Magnum/MeshTools/CompressIndices.h>
 #include <Magnum/MeshTools/Interleave.h>
 #include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Primitives/Axis.h>
 #include <Magnum/Primitives/Cube.h>
 #include <Magnum/Primitives/Square.h>
 #include <Magnum/Shaders/Phong.h>
@@ -23,11 +24,12 @@ class CompGeom : public Platform::Application {
   private:
     void drawEvent() override;
 
-    GL::Mesh _mesh;
-    Shaders::Phong _shader;
+    GL::Mesh axis_{NoCreate};
+    GL::Mesh point_{NoCreate};
+    Shaders::Phong shader_;
 
-    Matrix4 _transformation, _projection;
-    Color3 _color;
+    Matrix4 projection_;
+    Color3 color_;
 };
 
 CompGeom::CompGeom(const Arguments& arguments)
@@ -35,52 +37,36 @@ CompGeom::CompGeom(const Arguments& arguments)
     GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
     GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
 
-    //Trade::MeshData cube = Primitives::cubeSolid();
-    //Trade::MeshData point = Primitives::squareSolid();
+    // Initialise the meshes.
+    axis_ = MeshTools::compile(Primitives::axis3D());
+    point_ = MeshTools::compile(Primitives::squareSolid());
 
-    /*GL::Buffer vertices;
-    vertices.setData(MeshTools::interleave(cube.positions3DAsArray(),
-                                           cube.normalsAsArray()));
+    // Configure shader.
+    color_ = Color3::fromHsv({35.0_degf, 1.0f, 1.0f});
 
-    std::pair<Containers::Array<char>, MeshIndexType> compressed =
-        MeshTools::compressIndices(cube.indicesAsArray());
-    GL::Buffer indices;
-    indices.setData(compressed.first);
-
-    _mesh.setPrimitive(cube.primitive())
-        .setCount(cube.indexCount())
-        .addVertexBuffer(std::move(vertices), 0, Shaders::Phong::Position{},
-                         Shaders::Phong::Normal{})
-        .setIndexBuffer(std::move(indices), 0, compressed.second);*/
-
-    // This is a verbose version of:
-    //_mesh = MeshTools::compile(Primitives::cubeSolid());
-
-
-    _mesh = MeshTools::compile(Primitives::squareSolid());
-
-    _transformation =
-        Matrix4::rotationX(30.0_degf) * Matrix4::rotationY(40.0_degf);
-    _projection =
+    projection_ =
         Matrix4::perspectiveProjection(
             35.0_degf, Vector2{windowSize()}.aspectRatio(), 0.01f, 100.0f) *
-        Matrix4::translation(Vector3::zAxis(-10.0f));
-    _color = Color3::fromHsv({35.0_degf, 1.0f, 1.0f});
+        Matrix4::translation(Vector3::zAxis(-20.0f));
+
+    shader_.setLightPositions({{7.0f, 5.0f, 2.5f, 0.0f}})
+        .setProjectionMatrix(projection_);
 }
 
 void CompGeom::drawEvent() {
     GL::defaultFramebuffer.clear(GL::FramebufferClear::Color |
                                  GL::FramebufferClear::Depth);
-
-    //_shader.setLightPositions({{7.0f, 5.0f, 2.5f, 0.0f}}).draw(_mesh);
-
-    _shader.setLightPositions({{7.0f, 5.0f, 2.5f, 0.0f}})
-        .setDiffuseColor(_color)
-        .setAmbientColor(Color3::fromHsv({_color.hue(), 1.0f, 0.3f}))
-        //.setTransformationMatrix(_transformation)
-        //.setNormalMatrix(_transformation.normalMatrix())
-        .setProjectionMatrix(_projection)
-        .draw(_mesh);
+    // Render Axis and Points
+    shader_.draw(axis_);
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            shader_.setTransformationMatrix(
+                Matrix4::translation(Vector3(i, j, 0)));
+            shader_.setDiffuseColor(color_);
+            shader_.setAmbientColor(Color3::fromHsv({color_.hue(), 1.0f, 0.3f}));
+            shader_.draw(point_);
+        }
+    }
 
     swapBuffers();
 }
